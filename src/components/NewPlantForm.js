c6ce62ff9a4f654666acfd8e71401918219856cf
Bianca Aspin <bianca@mj94.net>
2023-02-08 09:48:20 -0800

    Set up plant form as a controlled component.

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 function NewPlantForm() {
+
+  const formObject = {
+    name: "",
+    image: "",
+    price: "",
+  }
+
+  const [formData, setFormData] = useState(formObject);
+
+  function handleChange(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+    const updatedFormObject = {
+      ...formData,
+      [key]: value
+    }
+    setFormData(updatedFormObject);
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+        <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={handleChange}/>
+        <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleChange}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" value={formData.price} onChange={handleChange}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
